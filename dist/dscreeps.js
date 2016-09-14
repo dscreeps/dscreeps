6f1a1f69fd7f31bd47cce77ba8f8d5c55350fa0e
Christoph Herrmann <christoph.herrmann@maxdome.de>
2016-09-14 16:51:00 +0200

    Transpile

--- dist/dscreeps.js
+++ dist/dscreeps.js
@@ -98,6 +98,34 @@ function getDynamicSourceData(source) {
   };
 }
 
+function getHarvestPositions(source) {
+  const objects = source.room.lookAtArea(
+    source.pos.y - 1,
+    source.pos.x - 1,
+    source.pos.y + 1,
+    source.pos.x + 1
+  );
+  const harvestPositions = [];
+  _.each([source.pos.y - 1, source.pos.y, source.pos.y + 1], y => {
+    _.each([source.pos.x - 1, source.pos.x, source.pos.x + 1], x => {
+      if (y === source.pos.y && x === source.pos.x) {
+        return;
+      }
+      let foundWall = false;
+      _.each(objects[y][x], object => {
+        if (object.type === 'terrain' && object.terrain === 'wall') {
+          foundWall = true;
+        }
+      });
+      if (foundWall) {
+        return;
+      }
+      harvestPositions.push({ x: x, y: y });
+    });
+  });
+  return harvestPositions;
+}
+
 function getSourceDatas() {
   Game.Memory.sourceDatas = Game.Memory.sourceDatas || {};
   return Game.Memory.sourceDatas;
@@ -105,6 +133,7 @@ function getSourceDatas() {
 
 function getStaticSourceData(source) {
   return {
+    harvestPositions: getHarvestPositions(source),
     id: source.id
   };
 }
